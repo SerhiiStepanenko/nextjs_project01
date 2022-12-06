9b4d7bb4925c66aca4e836cc69b31082d452e51f
Serhii Stepanenko <stepanenkos821@gmail.com>
2022-12-06 09:05:36 +0200

    save project01

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading"
+
+const Footer = () => {
+    return (
+        <footer>
+            <Heading text='Footer' />
+        </footer>
+    )
+
+}
+
+export default Footer
